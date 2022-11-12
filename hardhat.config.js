7a35a84961686f81e4d2df4173b342197fe9d162
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2022-11-11 17:31:50 -0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,9 +3,9 @@
  */
 
 require("@nomiclabs/hardhat-ethers");
-ALCHEMY_API_KEY = "WDx_Z8DYzJ2lyAOwr7CHfL647hhPEqnF";
+ALCHEMY_API_KEY = [{process.env}];
 
-PRIVATE_KEY = "7ef15be65184ed5a15c82dd11aa5fdf5fe93b59e05d2c66c2d59186918a628eb"
+PRIVATE_KEY = "*************************************************************"
  module.exports = {
     solidity: "0.8.9",
     defaultNetwork: "mumbai",
